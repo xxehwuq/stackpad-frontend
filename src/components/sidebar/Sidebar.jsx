5de2a19d7d354a86cac9d3d421f465b884fe5a21
Yaroslav Yarosh <yaroslavyarosh@protonmail.com>
2022-06-16 21:43:18 +0300

    -

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,12 +3,13 @@ import styles from './Sidebar.module.sass'
 import SidebarItem from './SidebarItem'
 import SidebarGroup from './SidebarGroup'
 import SidebarLetter from './SidebarLetter';
+import logo from "../../images/logo.svg"
 
 function Sidebar() {
     return (
         <div className={styles.Sidebar}>
             <SidebarGroup>
-                <img className={styles.SidebarLogo} src='https://img.icons8.com/fluency/344/note.png' alt='logo'/>
+                <img className={styles.SidebarLogo} src={logo} alt='logo'/>
                 <SidebarItem to="notebooks"><BookOpen/></SidebarItem>
                 <SidebarItem to="bookmarks"><Bookmark/></SidebarItem>
             </SidebarGroup>
--- src/components/sidebar/SidebarLetter.jsx
+++ src/components/sidebar/SidebarLetter.jsx
@@ -3,11 +3,9 @@ import styles from './Sidebar.module.sass'
 
 function SidebarLetter({children}) {
     const to = "/u/notebooks/letter/" + children
-    let resolved = useResolvedPath(to);
-    let match = useMatch({ path: resolved.pathname, end: false });
 
     return (
-        <Link className={match ? `${styles.SidebarLetter} ${styles.SidebarLetterActive}` : styles.SidebarLetter} to={to}>{children}</Link>
+        <Link className={styles.SidebarLetter} to={to}>{children}</Link>
     );
 }
 
